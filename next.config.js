890d88c018cdce07049b9b1fed1b9a46ce3a2d2d
omache <omacherenox@gmail.com>
2023-05-29 07:44:29 +0300

    check for any changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// Check for this comment then everything is okay
