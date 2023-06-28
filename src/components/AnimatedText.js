ff5fe6cf70bb6a008d0800248cf19cf695901704
omache <omacherenox@gmail.com>
2023-06-28 10:02:11 +0300

    font changes

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,56 +1,54 @@
-import React from 'react'
-import {motion} from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 
-const quote ={
-  initial:{
-    opacity:1,
+const quote = {
+  initial: {
+    opacity: 1,
   },
-  animate:{
-    opacity:1,
-    transition:{
-      delay:0.5,
-      staggerChildren:0.08,
-    }
-  }
-}
-const singlWord ={
-  initial:{
-    opacity:0,
-    y:50,
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.08,
+    },
   },
-  animate:{
-    opacity:1,
-    y:0,
-    transition:{
-      duration:1,
-    }
-  }
-}
-const AnimatedText = ({text, className=""}) => {
+};
+
+const singlWord = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden sm:py-0
-    
-    '>
-      <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl  dark:text-light
-      ${className}`}
-      variants={quote}
-      initial="initial"
-      animate="animate"
+    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden sm:py-0">
+      <motion.h1
+        className={`inline-block w-full text-dark font-bold capitalize text-8xl dark:text-light ${className}`}
+        variants={quote}
+        initial="initial"
+        animate="animate"
       >
-
-        {
-        text.split("").map((word,index) =>
-        <motion.span key={word+'-'+index} className='inline-block'
-        variants={singlWord}
-        >
-          {word}&nbsp;
-        </motion.span>
-        )
-        }
-      
+        {text.split(" ").map((word, index) => (
+          <motion.span
+            key={word + "-" + index}
+            className="inline-block"
+            variants={singlWord}
+          >
+            {word}&nbsp;
+          </motion.span>
+        ))}
       </motion.h1>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default AnimatedText
+export default AnimatedText;
